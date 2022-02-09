3cddfa99f8aa6cd86cecde8b855335c165deb43a
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2022-02-10 00:39:09 +0200

    user class and attack

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hashlib
 import time
+import copy
 
 class Node:
     def __init__(self, data):
@@ -60,8 +61,14 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.generate_genesis_block()
-        self.difficulty=4
+        self.difficulty=5
         self.transactions_list=[]
+
+    def set_difficulty(self,n):
+        self.difficulty=n
+
+    def get_difficulty(self):
+        return self.difficulty
 
     def generate_genesis_block(self):
         genesis_node=Node("Genesis Block")
@@ -72,8 +79,7 @@ class Blockchain:
             self.transactions_list.append(transaction)
         elif len(transaction)>1:
             for i in range(len(transaction)):
-                self.transactions_list.append(transaction[i])
-        print(len(self.transactions_list))
+                self.transactions_list.append(transaction[i]) 
         if len(self.transactions_list)==8:
             root=Markle_tree(self.transactions_list).root_node()
             previous_block_hash = self.last_block.block_hash
@@ -92,7 +98,33 @@ class Blockchain:
     def last_block(self):
         return self.chain[-1]
 
+
+class user:
+    def __init__(self,id):
+        self.id = id
+        self.local_block_chain = Blockchain()
+
+    def set_BC_block_chain(self,bc):
+        self.compare(bc)
+    
+    def compare(self,bc):
+        if len(bc.chain)>len(self.local_block_chain.chain):
+            self.local_block_chain = copy.deepcopy(bc) 
+        elif len(bc.chain)==len(self.local_block_chain.chain):
+            if((bc.chain[-1].timestamp-self.local_block_chain.chain[-1].timestamp)<0):
+                self.local_block_chain = copy.deepcopy(bc)
+
+
+
+
 if __name__ == "__main__":
+
+    user1 = user(1)
+    user2 = user(2)
+    attacker = user(-1)
+    user3 = user(3)
+
+
 
     t1 = Transaction("George", "Joe", "3.1")
     t2 = Transaction("Joe", "Adam", "2.5")
@@ -102,20 +134,52 @@ if __name__ == "__main__":
     t6 = Transaction("Ji", "Adam", "2.5")
     t7 = Transaction("Jet", "Adam", "2.5")
     t8 = Transaction("Jel", "Adam", "2.5")
+    t9 = Transaction("George", "Joe", "3.1")
+    t10 = Transaction("Joe", "Adam", "2.5")
+    t11 = Transaction("Joe", "Bob", "2.5")
+    t12 = Transaction("Jo", "Adam", "2.5")
+    t13 = Transaction("Joee", "Adam", "2.5")
+    t14 = Transaction("Ji", "Adam", "2.5")
+    t15 = Transaction("Jet", "Adam", "2.5")
+    t16 = Transaction("Jel", "Adam", "2.5")
 
-    block_chain = Blockchain()
     start=time.time()
-    block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
-    block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value()])
-    block_chain.create_block_from_transaction(t8.hash_value())
+
+    user1.local_block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
+    user1.local_block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value()])
+    user1.local_block_chain.create_block_from_transaction(t8.hash_value())
+
+    user2.set_BC_block_chain(user1.local_block_chain)
+    attacker.set_BC_block_chain(user1.local_block_chain)
+    user3.set_BC_block_chain(user1.local_block_chain)
 
     end1=time.time()
     print(end1-start)
 
-    block_chain.display_chain()
-    block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value(), t8.hash_value()])
-    block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
+    user2.local_block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value(), t8.hash_value()])
+    user2.local_block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
+
+    user1.set_BC_block_chain(user2.local_block_chain)
+    attacker.set_BC_block_chain(user2.local_block_chain)
+    user3.set_BC_block_chain(user2.local_block_chain)
 
     end2=time.time()
     print(end2-end1)
-    block_chain.display_chain()
+
+    attacker.local_block_chain.display_chain()
+    attacker.local_block_chain.chain.pop()
+    attacker.local_block_chain.display_chain()
+    attacker.local_block_chain.create_block_from_transaction([t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value(),t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value()])
+    user1.set_BC_block_chain(attacker.local_block_chain)
+    user2.set_BC_block_chain(attacker.local_block_chain)
+    user3.set_BC_block_chain(attacker.local_block_chain)
+
+    user3.local_block_chain.create_block_from_transaction([t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value(),t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value()])
+    user1.set_BC_block_chain(user3.local_block_chain)
+    user2.set_BC_block_chain(user3.local_block_chain)
+    attacker.set_BC_block_chain(user3.local_block_chain)
+    print('check')
+    user1.local_block_chain.display_chain()
+    user2.local_block_chain.display_chain()
+    user3.local_block_chain.display_chain()
+    attacker.local_block_chain.display_chain()
