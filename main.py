0cda3af8d6203900ee6fa7ba087ab71ed430ac5e
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2022-02-10 21:26:37 +0200

    Test cases

--- main.py
+++ main.py
@@ -49,20 +49,33 @@ class Block:
         self.block_data = f"{root.data} - {previous_block_hash}-{timestamp}-{self.nonce}"
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
-    def mineBlock(self, difficulty):
-        x="0"*difficulty
-        while (self.block_hash[:difficulty] != x):
-            self.nonce = self.nonce + 1
-            self.block_data = f"{self.root} - {self.previous_block_hash}-{self.timestamp}-{self.nonce}"
-            self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
-        print(self.nonce)
+
+class Pool:
+    def __init__(self):
+        self.pool = []
+
+    def add_transaction(self,transaction):
+        if type(transaction)==str:
+            self.pool.append(transaction)
+        elif len(transaction)>1:
+            for i in range(len(transaction)):
+                self.pool.append(transaction[i]) 
+
+    def generate_block(self):
+        if len(self.pool)>=8:
+            root=Markle_tree(self.pool).root_node()
+            t=time.time()
+            self.pool=self.pool[8:]
+            return Block("", root, t)
+        else:
+            return False
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.generate_genesis_block()
-        self.difficulty=5
-        self.transactions_list=[]
+        self.difficulty=4
+
 
     def set_difficulty(self,n):
         self.difficulty=n
@@ -74,20 +87,20 @@ class Blockchain:
         genesis_node=Node("Genesis Block")
         self.chain.append(Block("0", genesis_node, 0))
 
-    def create_block_from_transaction(self, transaction):
-        if type(transaction)==str:
-            self.transactions_list.append(transaction)
-        elif len(transaction)>1:
-            for i in range(len(transaction)):
-                self.transactions_list.append(transaction[i]) 
-        if len(self.transactions_list)==8:
-            root=Markle_tree(self.transactions_list).root_node()
-            previous_block_hash = self.last_block.block_hash
-            t=time.time()
-            B=Block(previous_block_hash, root, t)
-            B.mineBlock(self.difficulty)
-            self.chain.append(B)
-            self.transactions_list=[]
+    
+    def mineBlock(self,B,speed):
+        x="0"*self.difficulty
+        i=0
+        for i in range(speed):
+            if B.block_hash[:self.difficulty] == x:
+                print('nonce: ' + str(B.nonce))
+                self.chain.append(B)
+                return True
+            B.nonce = B.nonce + 1
+            B.block_data = f"{B.root.data} - {B.previous_block_hash}-{B.timestamp}-{B.nonce}"
+            B.block_hash = hashlib.sha256(B.block_data.encode()).hexdigest()
+        return False
+        
 
     def display_chain(self):
         for i in range(len(self.chain)-1):
@@ -99,7 +112,7 @@ class Blockchain:
         return self.chain[-1]
 
 
-class user:
+class User:
     def __init__(self,id):
         self.id = id
         self.local_block_chain = Blockchain()
@@ -114,72 +127,136 @@ class user:
             if((bc.chain[-1].timestamp-self.local_block_chain.chain[-1].timestamp)<0):
                 self.local_block_chain = copy.deepcopy(bc)
 
-
-
+    def create_block(self,b):
+        b.previous_block_hash = self.local_block_chain.chain[-1].block_hash
+        b1 = copy.deepcopy(b)
+        return b1
 
 if __name__ == "__main__":
 
-    user1 = user(1)
-    user2 = user(2)
-    attacker = user(-1)
-    user3 = user(3)
+    user = User(1)
+    user1 = User(2)
+    attacker = User(-1)
+    user2 = User(3)
 
+    transactions = Pool()
 
+    t = []
+    for i in range(40):
+        transactions.add_transaction(Transaction("joe","bob",str(i+1)).hash_value())
 
-    t1 = Transaction("George", "Joe", "3.1")
-    t2 = Transaction("Joe", "Adam", "2.5")
-    t3 = Transaction("Joe", "Bob", "2.5")
-    t4 = Transaction("Jo", "Adam", "2.5")
-    t5 = Transaction("Joee", "Adam", "2.5")
-    t6 = Transaction("Ji", "Adam", "2.5")
-    t7 = Transaction("Jet", "Adam", "2.5")
-    t8 = Transaction("Jel", "Adam", "2.5")
-    t9 = Transaction("George", "Joe", "3.1")
-    t10 = Transaction("Joe", "Adam", "2.5")
-    t11 = Transaction("Joe", "Bob", "2.5")
-    t12 = Transaction("Jo", "Adam", "2.5")
-    t13 = Transaction("Joee", "Adam", "2.5")
-    t14 = Transaction("Ji", "Adam", "2.5")
-    t15 = Transaction("Jet", "Adam", "2.5")
-    t16 = Transaction("Jel", "Adam", "2.5")
+    b = transactions.generate_block()
+    b1 = user1.create_block(b)
+    b2 = user2.create_block(b)
 
-    start=time.time()
+    start_time = time.time()
 
-    user1.local_block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
-    user1.local_block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value()])
-    user1.local_block_chain.create_block_from_transaction(t8.hash_value())
+    while(1):   
+        if user1.local_block_chain.mineBlock(b1,51):
+            user.set_BC_block_chain(user1.local_block_chain)
+            user2.set_BC_block_chain(user1.local_block_chain)
+            attacker.set_BC_block_chain(user1.local_block_chain)
+            print("block 1: User1 is TOP\n")
+            break
+        elif user2.local_block_chain.mineBlock(b2,49):
+            user.set_BC_block_chain(user2.local_block_chain)
+            user1.set_BC_block_chain(user2.local_block_chain)
+            attacker.set_BC_block_chain(user2.local_block_chain)
+            print("block 1: User2 is TOP\n")
+            break
 
-    user2.set_BC_block_chain(user1.local_block_chain)
-    attacker.set_BC_block_chain(user1.local_block_chain)
-    user3.set_BC_block_chain(user1.local_block_chain)
+    print(f'time taken to mine block = {time.time()-start_time} sec')
+    b = transactions.generate_block()
 
-    end1=time.time()
-    print(end1-start)
+    b1 = user1.create_block(b)
+    b2 = user2.create_block(b)
 
-    user2.local_block_chain.create_block_from_transaction([t5.hash_value(), t6.hash_value(), t7.hash_value(), t8.hash_value()])
-    user2.local_block_chain.create_block_from_transaction([t1.hash_value(), t2.hash_value(), t3.hash_value(), t4.hash_value()])
+    while(1):   
+        if user2.local_block_chain.mineBlock(b2,51):
+            user.set_BC_block_chain(user2.local_block_chain)
+            user1.set_BC_block_chain(user2.local_block_chain)
+            attacker.set_BC_block_chain(user2.local_block_chain)
+            print("block 2: User2 is TOP\n")
+            break
 
-    user1.set_BC_block_chain(user2.local_block_chain)
-    attacker.set_BC_block_chain(user2.local_block_chain)
-    user3.set_BC_block_chain(user2.local_block_chain)
+        elif user1.local_block_chain.mineBlock(b1,49):
+            user.set_BC_block_chain(user1.local_block_chain)
+            user2.set_BC_block_chain(user1.local_block_chain)
+            attacker.set_BC_block_chain(user1.local_block_chain)
+            print("block 2: User1 is TOP\n")
+            break
 
-    end2=time.time()
-    print(end2-end1)
+    print('after adding 2 blocks: ')
+    user.local_block_chain.display_chain()
 
-    attacker.local_block_chain.display_chain()
+    b = transactions.generate_block()
+
+    b1 = user1.create_block(b)
+    b2 = user2.create_block(b)
     attacker.local_block_chain.chain.pop()
+    print("attacker: ")
     attacker.local_block_chain.display_chain()
-    attacker.local_block_chain.create_block_from_transaction([t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value(),t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value()])
-    user1.set_BC_block_chain(attacker.local_block_chain)
-    user2.set_BC_block_chain(attacker.local_block_chain)
-    user3.set_BC_block_chain(attacker.local_block_chain)
 
-    user3.local_block_chain.create_block_from_transaction([t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value(),t9.hash_value(), t10.hash_value(), t11.hash_value(), t12.hash_value()])
-    user1.set_BC_block_chain(user3.local_block_chain)
-    user2.set_BC_block_chain(user3.local_block_chain)
-    attacker.set_BC_block_chain(user3.local_block_chain)
-    print('check')
-    user1.local_block_chain.display_chain()
-    user2.local_block_chain.display_chain()
-    user3.local_block_chain.display_chain()
+    while(1):   
+        if attacker.local_block_chain.mineBlock(b1,40):
+            user.set_BC_block_chain(attacker.local_block_chain)
+            user1.set_BC_block_chain(attacker.local_block_chain)
+            user2.set_BC_block_chain(attacker.local_block_chain)
+            print("block 3: Attacker is TOP\n")
+            break
+        elif user2.local_block_chain.mineBlock(b2,49):
+            user.set_BC_block_chain(user2.local_block_chain)
+            user1.set_BC_block_chain(user2.local_block_chain)
+            attacker.set_BC_block_chain(user2.local_block_chain)
+            print("block 3: User2 is TOP\n")
+            break
+
+    print('block chain: ')
+    user.local_block_chain.display_chain()
+
+    print('forked block chain: ')
     attacker.local_block_chain.display_chain()
+
+    b = transactions.generate_block()
+
+    b1 = user1.create_block(b)
+    b2 = user2.create_block(b)
+
+    while(1):   
+        if attacker.local_block_chain.mineBlock(b1,40):
+            user.set_BC_block_chain(attacker.local_block_chain)
+            user1.set_BC_block_chain(attacker.local_block_chain)
+            user2.set_BC_block_chain(attacker.local_block_chain)
+            print("block 4: Attacker is TOP\n")
+            break
+        elif user2.local_block_chain.mineBlock(b2,49):
+            user.set_BC_block_chain(user2.local_block_chain)
+            user1.set_BC_block_chain(user2.local_block_chain)
+            attacker.set_BC_block_chain(user2.local_block_chain)
+            print("block 4: User2 is TOP\n")
+            break
+
+    print("after attack: ")
+    user.local_block_chain.display_chain()
+
+    b = transactions.generate_block()
+
+    b1 = user1.create_block(b)
+    b2 = user2.create_block(b)
+
+    while(1):   
+        if user1.local_block_chain.mineBlock(b1,51):
+            user.set_BC_block_chain(user1.local_block_chain)
+            attacker.set_BC_block_chain(user1.local_block_chain)
+            user2.set_BC_block_chain(user1.local_block_chain)
+            print("block 5: User1 is TOP\n")
+            break
+        elif user2.local_block_chain.mineBlock(b2,49):
+            user.set_BC_block_chain(user2.local_block_chain)
+            user1.set_BC_block_chain(user2.local_block_chain)
+            attacker.set_BC_block_chain(user2.local_block_chain)
+            print("block 5: User2 is TOP\n")
+            break
+    
+    print("trusted block chain: ")
+    user.local_block_chain.display_chain()
